5450bed26dd90b99f6c3f0f9217a5eadcbaf1fd4
alexander furkach <furkach93@gmail.com>
2022-06-21 10:16:10 +0300

    Update params.php

--- config/params.php
+++ config/params.php
@@ -7,6 +7,6 @@ return [
     'currencies_all' => ['select currency', 'BTC', 'ETH', 'DOGE', 'LTC', 'USD'],
     'currencies_money' => ['USD'], 
     'currencies_crypto' => ['BTC', 'ETH', 'DOGE', 'LTC'], 
-    'API_KEY' => '6e95769cd14b489da50c078dd73540a2', 
+    'API_KEY' => '539ad2a353da0280c49d725c15865614', 
     'api_url' => 'http://api.coinlayer.com/live?access_key='
 ];
